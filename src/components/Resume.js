a755dd3b61292dcf820e66107b977a5e8c55bf25
JoeG21 <jcggomez@live.com>
2021-05-20 19:04:05 -0500

    update about

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,9 +9,9 @@ const Resume = () => {
                 title="Joe C Gomez's Resume"
                 className='resume'
                 frameBorder='0'
-                src='https://drive.google.com/file/d/1jDKtZNqkVXzcPC2dm1-Y-IqxCJ8YoNHH/preview'>
+                src='https://drive.google.com/file/d/1AjAt3WqBBGcpeiL7tO0RpivGlJVduVmg/preview'>
             </iframe>
-            <a href='https://drive.google.com/uc?export=download&id=1jDKtZNqkVXzcPC2dm1-Y-IqxCJ8YoNHH'>
+            <a href='https://drive.google.com/uc?export=download&id=1AjAt3WqBBGcpeiL7tO0RpivGlJVduVmg'>
                 <div className='download-container'>
                     <img src={download} alt='Download Resume' className='download' />
                     <p> Download </p>
